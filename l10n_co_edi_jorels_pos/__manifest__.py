72c72123783e1e9492e29fd5ebea63a15cbcf6b7
Jorge Sanabria <js@jorels.com>
2022-03-10 00:50:19 -0500

    Actualización de versión para el modulo de POS

--- l10n_co_edi_jorels_pos/__manifest__.py
+++ l10n_co_edi_jorels_pos/__manifest__.py
@@ -27,7 +27,7 @@
     'author': 'Jorels SAS',
     'license': 'LGPL-3',
     'category': 'Point of Sale',
-    'version': '13.0.0.1',
+    'version': '13.0.22.03.10.00.48',
     'website': 'https://www.jorels.com',
     'images': ['static/images/main_screenshot.png'],
     'support': 'info@jorels.com',
