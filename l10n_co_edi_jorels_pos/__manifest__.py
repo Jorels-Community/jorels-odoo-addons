21db7c7a16e34820df4595aa5d557b977c09bac2
Jorge Sanabria <js@jorels.com>
2022-07-07 03:56:57 -0500

    l10n_co_edi_jorels_pos: Necesario cambio de licencia dado que el módulo usa como dependencia un modulo de licencia AGPL, la licencia del módulo no puede ser LGPL. Se cambia por tanto a AGPL también. Esto según la recomendación de Odoo que puede leerse en https://apps.odoo.com/apps/faq#maintainer_faq_04

--- l10n_co_edi_jorels_pos/__manifest__.py
+++ l10n_co_edi_jorels_pos/__manifest__.py
@@ -1,23 +1,22 @@
 # -*- coding: utf-8 -*-
 #
-# Jorels S.A.S. - Copyright (2019-2022)
+#   l10n_co_edi_jorels_pos
+#   Copyright (C) 2022  Jorels SAS
 #
-# This file is part of l10n_co_edi_jorels_pos.
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published
+#   by the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
 #
-# l10n_co_edi_jorels_pos is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
 #
-# l10n_co_edi_jorels_pos is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-# You should have received a copy of the GNU Lesser General Public License
-# along with l10n_co_edi_jorels_pos.  If not, see <https://www.gnu.org/licenses/>.
-#
-# email: info@jorels.com
+#   email: info@jorels.com
 #
 
 {
@@ -25,7 +24,7 @@
     'summary': 'Facturación electrónica POS para Colombia por Jorels',
     'description': "Free POS electronic invoice for Colombia by Jorels",
     'author': 'Jorels SAS',
-    'license': 'LGPL-3',
+    'license': 'AGPL-3',
     'category': 'Point of Sale',
     'version': '15.0.22.07.07.01.03',
     'website': 'https://www.jorels.com',
