ee070c8fac3810a5d8c2cc2543301043977ee220
July Caballero <jc@jorels.com>
2022-07-19 05:56:25 -0500

    l10n_co_hr_payroll: Se agregan los campos nota y contrato a la nómina consolidada. Se quita el campo color, mientras se piensa bien cual es la mejor manera de usarlo. Ahora el campo many2many payslip_ids, se copia en las nóminas consolidadas; esto es importante para el buen funcionamiento de las notas de ajuste. Se organizó el código para hacer más fácil futuras actualizaciones. Se corrigen algunos bugs y se agregan algunas validaciones a la nómina consolidada. Se agrega la consulta de documento en la nómina consolidada. Se mejora la vista de lista de la nómina consolidada y la nómina normal. Otras mejoras varias.

--- l10n_co_hr_payroll/models/hr_payslip.py
+++ l10n_co_hr_payroll/models/hr_payslip.py
@@ -18,15 +18,12 @@
 #
 #   email: info@jorels.com
 #
-
-
 import calendar
 import json
 import logging
 from datetime import datetime, timedelta
 
 import requests
-
 from dateutil.relativedelta import relativedelta
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError
@@ -54,17 +51,16 @@ class HrPayslip(models.Model):
                                       readonly=True, states={'draft': [('readonly', False)]}, copy=True)
     payment_method_id = fields.Many2one(comodel_name="l10n_co_edi_jorels.payment_methods", string="Payment method",
                                         default=1, readonly=True, states={'draft': [('readonly', False)]}, copy=True)
-    accrued_total_amount = fields.Monetary("Accrued total", currency_field='currency_id', readonly=True, copy=True)
-    deductions_total_amount = fields.Monetary("Deductions total", currency_field='currency_id', readonly=True,
-                                              copy=True)
-    others_total_amount = fields.Monetary("Others total", currency_field='currency_id', readonly=True, copy=True)
+    accrued_total_amount = fields.Monetary("Accrued", currency_field='currency_id', readonly=True, copy=True)
+    deductions_total_amount = fields.Monetary("Deductions", currency_field='currency_id', readonly=True, copy=True)
+    others_total_amount = fields.Monetary("Others", currency_field='currency_id', readonly=True, copy=True)
     total_amount = fields.Monetary("Total", currency_field='currency_id', readonly=True, copy=True)
     currency_id = fields.Many2one('res.currency', string='Currency', readonly=False, compute='_compute_currency')
     earn_ids = fields.One2many('l10n_co_hr_payroll.earn.line', 'payslip_id', string='Earn lines', readonly=True,
                                copy=True, states={'draft': [('readonly', False)]})
     deduction_ids = fields.One2many('l10n_co_hr_payroll.deduction.line', 'payslip_id', string='Deduction lines',
                                     copy=True, readonly=True, states={'draft': [('readonly', False)]})
-    worked_days_total = fields.Integer("Worked days total", default=0)
+    worked_days_total = fields.Integer("Worked days", default=0)
 
     # Edi response fields
     edi_is_valid = fields.Boolean("Is valid?", copy=False)
@@ -102,8 +98,8 @@ class HrPayslip(models.Model):
                                        relation='hr_payslip_hr_payslip_edi_rel',
                                        readonly=True, copy=False)
 
-    color = fields.Integer(string='Color', required=False, default=None, readonly=True, compute='_compute_color',
-                           store=True, copy=False)
+    # color = fields.Integer(string='Color', required=False, default=None, readonly=True, compute='_compute_color',
+    #                        store=True, copy=False)
 
     month = fields.Selection([
         ('1', 'January'),
@@ -126,7 +122,7 @@ class HrPayslip(models.Model):
         hr_payslip_edi_env = self.env['hr.payslip.edi']
         for rec in self:
             if rec.edi_payload:
-                rec.edi_payload_html = hr_payslip_edi_env.json2html(json.loads(rec.edi_payload), 2)
+                rec.edi_payload_html = hr_payslip_edi_env.payload2html(json.loads(rec.edi_payload), 2)
             else:
                 rec.edi_payload_html = ""
 
@@ -318,7 +314,7 @@ class HrPayslip(models.Model):
             rec.others_total_amount = others_total_amount
             rec.total_amount = accrued_total_amount - deductions_total_amount
 
-            rec.edi_payload = json.dumps(self.get_json_request(), indent=4, sort_keys=False)
+            rec.edi_payload = json.dumps(rec.get_json_request(), indent=4, sort_keys=False)
 
         return res
 
@@ -1308,7 +1304,6 @@ class HrPayslip(models.Model):
             json_request['payment_dates'] = payment_dates
             json_request['earn'] = earn
 
-
             # Optionals
             if deduction:
                 json_request['deduction'] = deduction
@@ -1501,7 +1496,7 @@ class HrPayslip(models.Model):
         res = super(HrPayslip, self).action_payslip_done()
 
         for rec in self:
-            if rec.company_id.edi_payroll_enable or not rec.company_id.edi_payroll_consolidated_enable:
+            if rec.company_id.edi_payroll_enable and not rec.company_id.edi_payroll_consolidated_enable:
                 rec.validate_dian_generic()
 
         return res
@@ -1625,13 +1620,13 @@ class HrPayslip(models.Model):
             'context': {}
         }
 
-    @api.depends('state')
-    def _compute_color(self):
-        for rec in self:
-            switcher = {
-                'draft': 11,
-                'verify': 2,
-                'cancel': 1,
-                'done': 0
-            }
-            rec.color = switcher.get(rec.state, 11)
+    # @api.depends('state')
+    # def _compute_color(self):
+    #     for rec in self:
+    #         switcher = {
+    #             'draft': 11,
+    #             'verify': 2,
+    #             'cancel': 1,
+    #             'done': 0
+    #         }
+    #         rec.color = switcher.get(rec.state, 11)
