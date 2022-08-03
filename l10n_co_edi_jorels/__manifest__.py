a8ef98c81751c6059bbd8c293963211dadb616c7
Jorge Sanabria <js@jorels.com>
2022-08-03 14:08:00 -0500

    l10n_co_edi_jorels: Actualización de las tablas de la DIAN y actualización de la versión del módulo. Nos aseguramos que las tablas actualicen, dado recientes problemas con algunos usuarios en la instalación.

--- l10n_co_edi_jorels/__manifest__.py
+++ l10n_co_edi_jorels/__manifest__.py
@@ -27,7 +27,7 @@
     'author': "Jorels SAS",
     'license': "LGPL-3",
     'category': 'Invoicing & Payments',
-    'version': '13.0.22.07.14.0228',
+    'version': '13.0.22.08.03.14.06',
     'website': "https://www.jorels.com",
     'images': ['static/images/main_screenshot.png'],
     'support': 'info@jorels.com',
