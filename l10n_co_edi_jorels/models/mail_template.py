d060c22104797244008828e056d749facf393c43
Jorge Sanabria <js@jorels.com>
2023-01-20 02:32:18 -0500

    l10n_co_edi_jorels: Se cambia la plantilla de correo de la factura electrónica por una nueva plantilla personalizada especificamente para este proposito. Esto corrige el bug que existia cuando se actaulizaba el modulo contable de manera que podia quedar mal configurado el asunto de correo. Esto tambien nos permitio automatizar el envio de correo una vez se valida la factura en la DIAN. Si se desea enviar el correo nuevamente se puede usar el cuadro de dialogo de envio de correo y seleccionar la plantilla adecuada o usar una acción que se creo en el menu de acciones del modelo de facturas. Por compatibilidad no se borra la plantilla actual, pero se recomienda usar la nueva plantilla.

--- l10n_co_edi_jorels/models/mail_template.py
+++ l10n_co_edi_jorels/models/mail_template.py
@@ -41,74 +41,77 @@ class MailTemplate(models.Model):
             res_ids = [res_ids]
             multi_mode = False
 
-        if self.model not in ('account.move', 'l10n_co_edi_jorels.radian'):
-            return res
-
         records = self.env[self.model].browse(res_ids)
 
         if self.model == 'account.move':
             for move in records:
-                record_data = (res[move.id] if multi_mode else res)
+                inv_default_template = self.env.ref('account.email_template_edi_invoice')
+                ei_template = self.env.ref('l10n_co_edi_jorels.email_template_edi')
+                if self.id in (ei_template.id, inv_default_template.id):
+                    if not move.company_id.ei_enable:
+                        continue
 
-                if not move.company_id.ei_enable:
-                    continue
+                    res_t = multi_mode and res[move.id] or res
 
-                if move.ei_is_valid \
-                        and move.move_type in ('out_invoice', 'out_refund') \
-                        and move.state not in ('draft', 'validate')\
-                        and move.ei_uuid:
+                    if 'attachments' not in res_t:
+                        res_t['attachments'] = []
+                    attachments = res_t['attachments'] if move.company_id.ei_include_pdf_attachment else []
 
-                    if move.ei_zip_name:
-                        attached_document_name = 'ad' + move.ei_zip_name[1:-4]
-                    else:
-                        attached_document_name = move.ei_uuid
+                    if move.is_to_send_edi_email():
+                        if move.ei_zip_name:
+                            attached_document_name = 'ad' + move.ei_zip_name[1:-4]
+                        else:
+                            attached_document_name = move.ei_uuid
 
-                    pdf_name = attached_document_name + '.pdf'
-                    pdf_path = Path(tempfile.gettempdir()) / pdf_name
+                        pdf_name = attached_document_name + '.pdf'
+                        pdf_path = Path(tempfile.gettempdir()) / pdf_name
 
-                    xml_name = attached_document_name + '.xml'
-                    xml_path = Path(tempfile.gettempdir()) / xml_name
+                        xml_name = attached_document_name + '.xml'
+                        xml_path = Path(tempfile.gettempdir()) / xml_name
 
-                    zip_name = attached_document_name + '.zip'
-                    zip_path = Path(tempfile.gettempdir()) / zip_name
+                        zip_name = attached_document_name + '.zip'
+                        zip_path = Path(tempfile.gettempdir()) / zip_name
 
-                    zip_archive = zipfile.ZipFile(zip_path, 'w')
+                        zip_archive = zipfile.ZipFile(zip_path, 'w')
 
-                    pdf_handle = open(pdf_path, 'wb')
-                    pdf_handle.write(base64.decodebytes(record_data["attachments"][0][1]))
-                    pdf_handle.close()
-                    zip_archive.write(pdf_path, arcname=pdf_name)
+                        pdf_handle = open(pdf_path, 'wb')
+                        pdf_handle.write(base64.decodebytes(res_t["attachments"][0][1]))
+                        pdf_handle.close()
+                        zip_archive.write(pdf_path, arcname=pdf_name)
 
-                    if move.ei_attached_document_base64_bytes:
                         xml_handle = open(xml_path, 'wb')
                         xml_handle.write(base64.decodebytes(move.ei_attached_document_base64_bytes))
                         xml_handle.close()
                         zip_archive.write(xml_path, arcname=xml_name)
 
-                    zip_archive.close()
+                        zip_archive.close()
 
-                    if move.ei_attached_document_base64_bytes:
                         with open(zip_path, 'rb') as f:
                             attached_zip = f.read()
                             ei_attached_zip_base64_bytes = base64.encodebytes(attached_zip)
-                            record_data.setdefault('attachments', [])
-                            if not move.company_id.ei_include_pdf_attachment:
-                                record_data['attachments'] = []
-                            record_data['attachments'].append((zip_name, ei_attached_zip_base64_bytes))
+                            attachments += [(zip_name, ei_attached_zip_base64_bytes)]
                             move.write({
                                 'ei_attached_zip_base64_bytes': ei_attached_zip_base64_bytes
                             })
 
-        if self.model == 'l10n_co_edi_jorels.radian':
-            for radian in records:
-                record_data = (res[radian.id] if multi_mode else res)
+                        res_t["attachments"] = attachments
 
+        elif self.model == 'l10n_co_edi_jorels.radian':
+            for radian in records:
                 if not radian.company_id.ei_enable:
                     continue
 
+                res_t = multi_mode and res[radian.id] or res
+
+                if 'attachments' not in res_t:
+                    res_t['attachments'] = []
+                attachments = res_t['attachments'] if radian.company_id.ei_include_pdf_attachment else []
+                # attachments = []
+
                 if radian.edi_is_valid \
                         and radian.state == 'posted'\
-                        and radian.edi_uuid:
+                        and radian.edi_uuid \
+                        and radian.edi_attached_document_base64:
 
                     if radian.edi_zip_name:
                         attached_document_name = 'ad' + radian.edi_zip_name[1:-4]
@@ -127,29 +130,25 @@ class MailTemplate(models.Model):
                     zip_archive = zipfile.ZipFile(zip_path, 'w')
 
                     # pdf_handle = open(pdf_path, 'wb')
-                    # pdf_handle.write(base64.decodebytes(res[res_id]["attachments"][0][1]))
+                    # pdf_handle.write(base64.decodebytes(res_t["attachments"][0][1]))
                     # pdf_handle.close()
                     # zip_archive.write(pdf_path, arcname=pdf_name)
 
-                    if radian.edi_attached_document_base64:
-                        xml_handle = open(xml_path, 'wb')
-                        xml_handle.write(base64.decodebytes(radian.edi_attached_document_base64))
-                        xml_handle.close()
-                        zip_archive.write(xml_path, arcname=xml_name)
+                    xml_handle = open(xml_path, 'wb')
+                    xml_handle.write(base64.decodebytes(radian.edi_attached_document_base64))
+                    xml_handle.close()
+                    zip_archive.write(xml_path, arcname=xml_name)
 
                     zip_archive.close()
 
-                    if radian.edi_attached_document_base64:
-                        with open(zip_path, 'rb') as f:
-                            attached_zip = f.read()
-                            edi_attached_zip_base64 = base64.encodebytes(attached_zip)
-                            record_data.setdefault('attachments', [])
-                            # if not radian.company_id.edi_include_pdf_attachment:
-                            if 'attachments' not in record_data:
-                                record_data['attachments'] = []
-                            record_data['attachments'].append((zip_name, edi_attached_zip_base64))
-                            radian.write({
-                                'edi_attached_zip_base64': edi_attached_zip_base64
-                            })
+                    with open(zip_path, 'rb') as f:
+                        attached_zip = f.read()
+                        edi_attached_zip_base64 = base64.encodebytes(attached_zip)
+                        attachments += [(zip_name, edi_attached_zip_base64)]
+                        radian.write({
+                            'edi_attached_zip_base64': edi_attached_zip_base64
+                        })
+
+                    res_t["attachments"] = attachments
 
         return res
