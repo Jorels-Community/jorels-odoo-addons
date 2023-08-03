9062138e03e0641debc9d0103c427c91f0be9e5d
Jorge Sanabria <js@jorels.com>
2023-08-02 22:13:21 -0500

    l10n_co_hr_payroll_enterprise: Se agrega el modelo de Inputs en la regla salarial, esto para poder relacionar las reglas salariales con las definiciones de "Otras entradas" de la nómina. Esto es extremadamente util en el caso de nómina electrónica, pues nos permite darle un tratamiento personalizado dependiente de la regla salarial relacionada a las entradas adicionales de devengados y deducciones de la nomina electrónica.

--- l10n_co_hr_payroll_enterprise/__manifest__.py
+++ l10n_co_hr_payroll_enterprise/__manifest__.py
@@ -28,7 +28,7 @@
     'author': 'Jorels SAS',
     'license': 'LGPL-3',
     'category': 'Human Resources',
-    'version': '14.0.23.05.27.05.06',
+    'version': '14.0.23.06.15.17.58',
     'website': "https://www.jorels.com",
     'images': ['static/images/main_screenshot.png'],
     'support': 'info@jorels.com',
