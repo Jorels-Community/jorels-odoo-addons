478103ea00b880c16ebf0292ece686ada6acdc63
Jorge Sanabria <js@jorels.com>
2023-09-22 10:00:39 -0500

    l10n_co_edi_jorels_pos: Similaridad de las ramas para facilitar las actualizaciones.

--- l10n_co_edi_jorels_pos/__manifest__.py
+++ l10n_co_edi_jorels_pos/__manifest__.py
@@ -1,33 +1,33 @@
 # -*- coding: utf-8 -*-
 #
-#   Jorels S.A.S. - Copyright (C) 2019-2023
+# Jorels S.A.S. - Copyright (2019-2022)
 #
-#   This file is part of l10n_co_edi_jorels_pos.
+# This file is part of l10n_co_edi_jorels_pos.
 #
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU Lesser General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
+# l10n_co_edi_jorels_pos is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU Lesser General Public License for more details.
+# l10n_co_edi_jorels_pos is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
 #
-#   You should have received a copy of the GNU Lesser General Public License
-#   along with this program. If not, see <https://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Lesser General Public License
+# along with l10n_co_edi_jorels_pos.  If not, see <https://www.gnu.org/licenses/>.
 #
-#   email: info@jorels.com
+# email: info@jorels.com
 #
 
 {
-    'name': 'Facturación electrónica POS para Colombia por Jorels',
-    'summary': 'Facturación electrónica POS para Colombia por Jorels',
+    'name': 'Free POS electronic invoice for Colombia by Jorels',
+    'summary': 'Free POS electronic invoice for Colombia by Jorels',
     'description': "Free POS electronic invoice for Colombia by Jorels",
     'author': 'Jorels SAS',
     'license': 'LGPL-3',
     'category': 'Point of Sale',
-    'version': '16.0.23.03.17.21.04',
+    'version': '16.0.22.07.23.02.58',
     'website': 'https://www.jorels.com',
     'images': ['static/images/main_screenshot.png'],
     'support': 'info@jorels.com',
