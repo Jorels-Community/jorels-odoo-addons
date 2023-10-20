414adb2dd38ac022cd23da89104ff473447e610d
Jorge Sanabria <js@jorels.com>
2023-10-20 16:52:12 -0500

    l10n_co_edi_jorels: Mejoras visuales y de usabilidad en las vistas de impuestos.

--- l10n_co_edi_jorels/__manifest__.py
+++ l10n_co_edi_jorels/__manifest__.py
@@ -61,6 +61,7 @@
         'views/mail_message_views.xml',
         'views/account_invoice_refund_view.xml',
         'views/radian_views.xml',
+        'views/listings/l10n_co_edi_jorels_taxes_view.xml',
         'report/report_invoice.xml',
         'data/mail_template_data.xml',
     ],
