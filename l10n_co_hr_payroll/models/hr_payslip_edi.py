a3ce58e1d3cfb01cc19459fdbee5deea56ea03f1
Jorge Sanabria <js@jorels.com>
2024-02-14 23:33:03 -0500

    l10n_co_hr_payroll: Se crea una accion de validacion masiva de nominas en Odoo, y una accion para la validacion masiva sobre la DIAN. Se corrige un bug que ocurre cuando se computan multiples registros para el campo nombre (name). Un documento enviado a la DIAN no se intentará validar de nuevo si ya está registrado como validado en la DIAN en el Odoo

--- l10n_co_hr_payroll/models/hr_payslip_edi.py
+++ l10n_co_hr_payroll/models/hr_payslip_edi.py
@@ -90,7 +90,7 @@ class HrPayslipEdi(models.Model):
             if (not rec.employee_id) or (not rec.month) or (not rec.year):
                 return
 
-            employee = self.employee_id
+            employee = rec.employee_id
 
             date_ym = dt.date(rec.year, int(rec.month), 1)
             locale = self.env.context.get('lang') or 'en_US'
@@ -268,17 +268,21 @@ class HrPayslipEdi(models.Model):
 
     def validate_dian_generic(self):
         for rec in self:
-            if not rec.company_id.edi_payroll_enable or not rec.company_id.edi_payroll_consolidated_enable:
+            if not rec.company_id.edi_payroll_enable or not rec.company_id.edi_payroll_consolidated_enable or rec.edi_is_valid:
                 continue
             requests_data = rec.get_json_request()
             rec._validate_dian_generic(requests_data)
 
     def validate_dian(self):
         for rec in self:
-            rec.validate_dian_generic()
+            if rec.state == 'done':
+                rec.validate_dian_generic()
 
     def action_payslip_done(self):
         for rec in self:
+            if rec.state != 'draft':
+                continue
+
             if not rec.number or rec.number in ('New', _('New')):
                 if rec.credit_note:
                     rec.number = self.env['ir.sequence'].next_by_code('salary.slip.edi.note')
