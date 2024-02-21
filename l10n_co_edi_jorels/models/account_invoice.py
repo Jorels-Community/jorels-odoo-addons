dca542961a8b6f630d9cc9deddd47946c8c58c29
Jorge Sanabria <js@jorels.com>
2024-02-21 13:57:23 -0500

    l10n_co_edi_jorels: El ministerio de transporte exige que para subir las facturas electrónicas al RNDC, los parametros adicionales deben estar ordenados. No es algo que este en el anexo tecnico, ni es algo realmente necesario estrictamente hablando, pero por imposición del ministerio de transporte si no se hace así se rechazara el documento al intentar subirlo al RNDC. Antes teniamos aquí como opcional el codigo de ingreso del documento devuelto por el ministerio, dado que este no aplicaba para remesas urbanas, pero ahora toda remesa debe tenerlo, así que tanto por la exigencia tecnica del ministerio de transporte como por la normativa del RNDC se hace necesario que ahora toda remesa que se suba, deba estar siempre con todos los parametros completos, de lo contrario no se podrá facturar por tipo de operación transporte.

--- l10n_co_edi_jorels/models/account_invoice.py
+++ l10n_co_edi_jorels/models/account_invoice.py
@@ -655,6 +655,9 @@ class AccountInvoice(models.Model):
                             # Check field values
                             if not invoice_line_id.waypoint_id.name_seq:
                                 raise UserError(_("A waypoint doesn't have an associated id number"))
+                            if not invoice_line_id.waypoint_id.rndc_ingresoid:
+                                raise UserError(_("Waypoint doesn't have an rndc ingress id: %s")
+                                                % invoice_line_id.waypoint_id.name_seq)
                             if not invoice_line_id.waypoint_id.total:
                                 raise UserError(_("The waypoint %s doesn't have an total")
                                                 % invoice_line_id.waypoint_id.name_seq)
@@ -671,6 +674,9 @@ class AccountInvoice(models.Model):
                             # "767","kilogram","KGM"
                             # "686","gallon","GLL"
                             item_properties = [{
+                                'name': '01',
+                                'value': invoice_line_id.waypoint_id.rndc_ingresoid
+                            }, {
                                 'name': '02',
                                 'value': invoice_line_id.waypoint_id.name_seq
                             }, {
@@ -679,15 +685,7 @@ class AccountInvoice(models.Model):
                                 'uom_code': '767',
                                 'quantity': invoice_line_id.waypoint_id.weight
                             }]
-
-                            if invoice_line_id.waypoint_id.rndc_ingresoid:
-                                item_properties.append({
-                                    'name': '01',
-                                    'value': invoice_line_id.waypoint_id.rndc_ingresoid
-                                })
-
                             invoice_temps.update({'item_properties': item_properties})
-
                         else:
                             # Additional service
                             invoice_temps.update({'sector_code': 1})
