19e87af02f0b626c3c152a4fa15df1f177b55a1a
Jorge Sanabria <js@jorels.com>
2024-04-25 00:32:40 -0500

    l10n_co_edi_jorels_pos: Actualizaciones necesarias para compatibilidad de POS en versión 17. Se actualizaron los modelos de python y se copiaron algunas funcionalidades existenten en versión 16, donde se habian corregido algunos bugs. TAmbien se actualizó la descripción web del modulo. Ya con esto se puede entrar a definir los cambios en las vistas xml del POS y posteriormente entrar a corregir bugs de javascript si es que surgen una vez completado este proceso

--- l10n_co_edi_jorels_pos/models/pos_config.py
+++ l10n_co_edi_jorels_pos/models/pos_config.py
@@ -28,32 +28,27 @@ class PosConfig(models.Model):
     _inherit = 'pos.config'
 
     def _default_electronic_invoice_journal(self):
-        return self.env['account.journal'].search([('type', '=', 'sale'), ('company_id', '=', self.env.company.id)],
-                                                  limit=1)
+        return self.env['account.journal'].search([
+            *self.env['account.journal']._check_company_domain(self.env.company),
+            ('type', '=', 'sale'),
+        ], limit=1)
 
     electronic_invoice_journal_id = fields.Many2one(
         'account.journal', string='Electronic Invoice Journal',
+        check_company=True,
         domain=[('type', '=', 'sale')],
         help="Accounting journal used to create electronic invoices.",
         default=_default_electronic_invoice_journal)
-
-    @api.constrains('company_id', 'electronic_invoice_journal_id')
-    def _check_company_electronic_invoice_journal(self):
-        for config in self:
-            if config.electronic_invoice_journal_id \
-                    and config.electronic_invoice_journal_id.company_id.id != config.company_id.id:
-                raise ValidationError(
-                    _("The electronic invoice journal and the point of sale %s must belong to its company.", config.name))
 
     @api.constrains('pricelist_id', 'use_pricelist', 'available_pricelist_ids', 'journal_id', 'invoice_journal_id',
                     'electronic_invoice_journal_id', 'payment_method_ids')
     def _check_currencies(self):
         super(PosConfig, self)._check_currencies()
 
-        if self.electronic_invoice_journal_id.currency_id \
-                and self.electronic_invoice_journal_id.currency_id.id != self.currency_id.id:
-            raise ValidationError(
-                _("The electronic invoice journal must be in the same currency as the Sales Journal or the company currency if that is not set."))
+        for config in self:
+            if config.electronic_invoice_journal_id.currency_id and config.electronic_invoice_journal_id.currency_id != config.currency_id:
+                raise ValidationError(
+                    _("The electronic invoice journal must be in the same currency as the Sales Journal or the company currency if that is not set."))
 
     def setup_defaults(self, company):
         super(PosConfig, self).setup_defaults(company)
@@ -62,7 +57,10 @@ class PosConfig(models.Model):
 
     def setup_electronic_invoice_journal(self, company):
         for pos_config in self:
-            electronic_invoice_journal_id = pos_config.electronic_invoice_journal_id or self.env[
-                'account.journal'].search([('type', '=', 'sale'), ('company_id', '=', company.id)], limit=1)
+            electronic_invoice_journal_id = (pos_config.electronic_invoice_journal_id or
+                                             self.env['account.journal'].search([
+                                                 *self.env['account.journal']._check_company_domain(company),
+                                                 ('type', '=', 'sale'),
+                                             ], limit=1))
             if electronic_invoice_journal_id:
                 pos_config.write({'electronic_invoice_journal_id': electronic_invoice_journal_id.id})
