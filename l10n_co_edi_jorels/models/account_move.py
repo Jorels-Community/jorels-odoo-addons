da8bb695c5ffa4092cef72e082c2471bc5904c3d
Jonathan Díaz <jonathan.diazm5@gmail.com>
2024-05-27 23:13:08 -0500

    l10n_co_edi_jorels: Se corrige la representación gráfica de de la factura electrónica.

--- l10n_co_edi_jorels/models/account_move.py
+++ l10n_co_edi_jorels/models/account_move.py
@@ -208,6 +208,12 @@ class AccountMove(models.Model):
                                       help="It indicates that the edi document has been sent.")
     
     same_currency_as_company = fields.Boolean(string='Same Currency as Company', compute='_compare_currencies')
+    
+    tax_totals_company_json = fields.Char(
+        string="Invoice Totals in company currency JSON",
+        compute='_compute_tax_totals_company_json',
+        readonly=False,
+        help='Edit Tax amounts if you encounter rounding issues.')
 
     def _auto_init(self):
         if not column_exists(self.env.cr, "account_move", "ei_type_document"):
@@ -1790,3 +1796,23 @@ class AccountMove(models.Model):
                 invoice.same_currency_as_company = True
             else:
                 invoice.same_currency_as_company = False
+                
+    @api.depends_context('lang')
+    @api.depends('line_ids.amount_currency', 'line_ids.tax_base_amount', 'line_ids.tax_line_id', 'partner_id', 'company_currency_id', 'amount_total_signed', 'amount_untaxed_signed')
+    def _compute_tax_totals_company_json(self):
+        """
+        Computed field used for custom widget's rendering.
+        Only set on invoices.
+        """
+        for move in self:
+            if not move.is_invoice(include_receipts=True):
+                # Non-invoice moves don't support that field (because of multicurrency: all lines of the invoice share the same currency)
+                move.tax_totals_company_json = None
+                continue
+
+            tax_lines_data = move._prepare_tax_lines_data_for_totals_from_invoice()
+
+            move.tax_totals_company_json = json.dumps({
+                **self._get_tax_totals(move.partner_id, tax_lines_data, move.amount_total_signed, move.amount_untaxed_signed, move.company_currency_id),
+                'allow_tax_edition': move.is_purchase_document(include_receipts=True) and move.state == 'draft',
+            })
