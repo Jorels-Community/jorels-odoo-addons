6e9a9dd6aad3be134067d821d482345e2ada6dd7
Jorge Sanabria <js@jorels.com>
2026-02-05 13:04:53 -0500

    [FIX] l10n_co_edi_jorels_pos: Fix QR code not appearing in PDF print
    The DIAN QR code was appearing in POS preview but not in PDF print.
    This was caused by using client-side JavaScript QR generation which
    doesn't execute during html-to-image conversion for PDF generation.
    
    Changes:
    - Replace JavaScript QRCode library with server-side barcode endpoint
    - Use Odoo's /report/barcode/ endpoint with barcode_type=QR
    - Set QR size to 256x256px with 100% width for better visibility
    - Remove unused qrcode.js library and its files
    
    The QR code now appears correctly in both preview and PDF print.

--- l10n_co_edi_jorels_pos/__manifest__.py
+++ l10n_co_edi_jorels_pos/__manifest__.py
@@ -21,43 +21,41 @@
 #
 
 {
-    'name': 'Free POS electronic invoice for Colombia by Jorels',
-    'summary': 'Free POS electronic invoice for Colombia by Jorels',
-    'description': "Free POS electronic invoice for Colombia by Jorels",
-    'author': 'Jorels SAS',
-    'license': 'LGPL-3',
-    'category': 'Point of Sale',
-    'version': '17.0.25.09.041305',
-    'website': 'https://www.jorels.com',
-    'images': ['static/images/main_screenshot.png'],
-    'support': 'info@jorels.com',
-    
+    "name": "Free POS electronic invoice for Colombia by Jorels",
+    "summary": "Free POS electronic invoice for Colombia by Jorels",
+    "description": "Free POS electronic invoice for Colombia by Jorels",
+    "author": "Jorels SAS",
+    "license": "LGPL-3",
+    "category": "Point of Sale",
+    "version": "17.0.25.09.041305",
+    "website": "https://www.jorels.com",
+    "images": ["static/images/main_screenshot.png"],
+    "support": "info@jorels.com",
     # Odoo and Jorels dependencies
-    'depends': [
-        'point_of_sale',
-        'l10n_co_edi_jorels',
+    "depends": [
+        "point_of_sale",
+        "l10n_co_edi_jorels",
     ],
-    'data': [
-        'views/res_config_settings_views.xml',
-        'views/pos_payment_method_views.xml',
+    "data": [
+        "views/res_config_settings_views.xml",
+        "views/pos_payment_method_views.xml",
     ],
-    'assets': {
-        'point_of_sale._assets_pos': [
-            'l10n_co_edi_jorels_pos/static/src/css/**/*',
+    "assets": {
+        "point_of_sale._assets_pos": [
+            "l10n_co_edi_jorels_pos/static/src/css/**/*",
             # 'l10n_co_edi_jorels_pos/static/src/js/**/*',
-            'l10n_co_edi_jorels_pos/static/src/js/models.js',
-            'l10n_co_edi_jorels_pos/static/src/js/pos_store.js',
-            'l10n_co_edi_jorels_pos/static/src/js/partner_editor.js',
-            'l10n_co_edi_jorels_pos/static/src/js/partner_list.js',
-            'l10n_co_edi_jorels_pos/static/src/js/payment_screen.js',
+            "l10n_co_edi_jorels_pos/static/src/js/models.js",
+            "l10n_co_edi_jorels_pos/static/src/js/pos_store.js",
+            "l10n_co_edi_jorels_pos/static/src/js/partner_editor.js",
+            "l10n_co_edi_jorels_pos/static/src/js/partner_list.js",
+            "l10n_co_edi_jorels_pos/static/src/js/payment_screen.js",
             # 'l10n_co_edi_jorels_pos/static/src/xml/**/*',
-            'l10n_co_edi_jorels_pos/static/src/xml/partner_editor.xml',
-            'l10n_co_edi_jorels_pos/static/src/xml/receipt_header.xml',
-            'l10n_co_edi_jorels_pos/static/src/xml/order_receipt.xml',
-            'l10n_co_edi_jorels_pos/static/src/xml/payment_screen.xml',
-            'l10n_co_edi_jorels_pos/static/lib/js/qrcode/qrcode.js',
+            "l10n_co_edi_jorels_pos/static/src/xml/partner_editor.xml",
+            "l10n_co_edi_jorels_pos/static/src/xml/receipt_header.xml",
+            "l10n_co_edi_jorels_pos/static/src/xml/order_receipt.xml",
+            "l10n_co_edi_jorels_pos/static/src/xml/payment_screen.xml",
         ],
     },
-    'installable': True,
-    'auto_install': True,
+    "installable": True,
+    "auto_install": True,
 }
